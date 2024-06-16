a1ecb4915f87f6c03bec2a61758d546514b2a313
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-06-16 20:23:12 +0530

    🚀dynamic-switch-button🖲️

--- components/DarkLight.js
+++ components/DarkLight.js
@@ -1,12 +1,28 @@
 "use client";
 
+import classNames from "classnames";
+import { useTheme } from "next-themes";
 import { FaCloudMoon } from "react-icons/fa";
+import { LuSunMoon } from "react-icons/lu";
 
 const DarkLight = () => {
+  const { theme, setTheme } = useTheme();
   return (
-    <div className="fixed top-4 right-4 w-12 h-[18px] rounded-full bg-zinc-700 flex items-center">
-      <span className="w-7 h-7 flex items-center justify-center bg-black rounded-full">
-        <FaCloudMoon className="text-white" size={14} />
+    <div
+      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      className="fixed top-4 right-4 w-12 h-[18px] rounded-full bg-violet-300 flex items-center"
+    >
+      <span
+        className={classNames(
+          "w-7 h-7 flex items-center justify-center bg-violet-700 rounded-full transition-all duration-500",
+          { "ml-5": theme === "dark" }
+        )}
+      >
+        {theme === "light" ? (
+          <FaCloudMoon className="text-white" size={14} />
+        ) : (
+          <LuSunMoon className="text-white" size={14} />
+        )}
       </span>
     </div>
   );
