c45c6152a3eebd76074ac835cb83d88ae2c9626b
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-06-27 17:47:20 +0530

    🎨styling-skills👨🏻‍💻

--- components/Skills.js
+++ components/Skills.js
@@ -1,10 +1,17 @@
+import data from "@/data/skills";
 import Heading from "./Heading";
 import SectionWrapper from "./SectionWrapper";
+import SkillComponent from "./SkillComponent";
 
 const Skills = () => {
   return (
     <SectionWrapper>
       <Heading>Skills</Heading>
+      <div className="flex flex-wrap items-center gap-5">
+        {data?.map((skill) => {
+          return <SkillComponent skill={skill} key={skill?.id} />;
+        })}
+      </div>
     </SectionWrapper>
   );
 };
