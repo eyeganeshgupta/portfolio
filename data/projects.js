41ccafc39149d527d9ac41b18d80da3404b32471
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-06-28 18:48:29 +0530

    projects-data

---
+++ data/projects.js
@@ -0,0 +1,69 @@
+const data = [
+  {
+    id: 1,
+    title: "CinemaBuff",
+    image: "/projects/cinema-buff.png",
+    description:
+      "Cinema-Buff, An ultimate destination for everything related to the world of cinema!",
+    skills: [
+      { id: 1, name: "react", image: "/skills/react.png" },
+      { id: 4, name: "typescript", image: "/skills/typescript.png" },
+      { id: 2, name: "next", image: "/skills/next-js.png" },
+      { id: 3, name: "tailwindcss", image: "/skills/tailwind.png" },
+      { id: 3, name: "node", image: "/skills/node-js.png" },
+      { id: 3, name: "mongodb", image: "/skills/mongodb.png" },
+    ],
+    source_code: "https://github.com/eyeganeshgupta/cinema-buff",
+    demo: "",
+  },
+  {
+    id: 2,
+    title: "Closet Heaven - An E-commerce Cloth Selling Web Application",
+    image: "/projects/closet-heaven.png",
+    description:
+      "Closet Heaven is an innovative E-commerce Cloth Selling Web Application designed to revolutionize the online fashion retail experience. This open-source project is committed to providing a comprehensive and customizable platform for entrepreneurs, and fashion enthusiasts.",
+    skills: [
+      { id: 1, name: "react", image: "/skills/react.png" },
+      { id: 4, name: "typescript", image: "/skills/typescript.png" },
+      { id: 2, name: "next", image: "/skills/next-js.png" },
+      { id: 3, name: "tailwindcss", image: "/skills/tailwind.png" },
+      { id: 3, name: "graphql", image: "/skills/graphql.png" },
+    ],
+    source_code: "https://github.com/eyeganeshgupta/closet-heaven-ui",
+    demo: "https://closet-heaven.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "Query Burst",
+    image: "/projects/query-burst.png",
+    description:
+      "An interactive hub where programming enthusiasts come together to seek assistance, exchange expertise, and engage in collaborative problem-solving. Dive into discussions spanning web development, mobile app creation, algorithms, data structures, and various other coding realms.",
+    skills: [
+      { id: 1, name: "react", image: "/skills/react.png" },
+      { id: 4, name: "typescript", image: "/skills/typescript.png" },
+      { id: 2, name: "next", image: "/skills/next-js.png" },
+      { id: 3, name: "tailwindcss", image: "/skills/tailwind.png" },
+      { id: 3, name: "graphql", image: "/skills/graphql.png" },
+    ],
+    source_code: "https://github.com/eyeganeshgupta/query-burst",
+    demo: "https://query-burst.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "Continental Hotel",
+    image: "/projects/continental-hotel.png",
+    description:
+      "“Continental Hotel” introduces a transformative digital platform poised to revolutionize the hotel booking landscape. With a keen focus on user experience and technological innovation, this application offers a seamless and efficient solution for travelers seeking accommodations.",
+    skills: [
+      { id: 1, name: "react", image: "/skills/react.png" },
+      { id: 4, name: "typescript", image: "/skills/typescript.png" },
+      { id: 2, name: "next", image: "/skills/next-js.png" },
+      { id: 3, name: "tailwindcss", image: "/skills/tailwind.png" },
+      { id: 3, name: "graphql", image: "/skills/graphql.png" },
+    ],
+    source_code: "https://github.com/eyeganeshgupta/continental-hotel",
+    demo: "https://continental-hotel-gupta.vercel.app/",
+  },
+];
+
+export default data;
