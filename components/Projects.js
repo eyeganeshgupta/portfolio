b75e9e74cc92d09e2bb6090afd0607b087849438
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-06-30 19:52:48 +0530

    🖼️image-aspect-ratio

--- components/Projects.js
+++ components/Projects.js
@@ -13,16 +13,16 @@ const Projects = () => {
             key={project.id}
             className="flex flex-wrap gap-5 w-full lg:w-8/12 mb-10"
           >
-            <div className="relative w-full md:w-[120px] h-[220px] md:h-20 rounded-lg overflow-hidden">
+            <div className="relative w-full md:w-[120px] pt-[56.25%] md:pt-0 md:h-20 rounded-lg overflow-hidden">
               <Image
                 src={project.image}
                 fill
-                className="w-full h-full"
+                className="w-full h-full absolute top-0 right-0 bottom-0 left-0"
                 alt={project.title}
               />
             </div>
             <div className="flex-1">
-              <h2 className="text-xl font-bold text-zinc-700 dark:text-zinc-400 leading-[25px]">
+              <h2 className="text-lg md:text-xl font-bold text-zinc-700 dark:text-zinc-400 leading-[25px]">
                 {project.title}
               </h2>
               <p className="text-base font-semibold text-zinc-500 dark:text-zinc-400 mt-2">
